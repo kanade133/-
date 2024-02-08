fce9cda09e371839abf4d051083357fefb86583f
wingsweiwei <147586151+wingsweiwei@users.noreply.github.com>
2024-02-08 14:57:25 +0800

    Clean, Add some Test Window

---
+++ Utils/ControlHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApplication4
+{
+    public static class ControlHelper
+    {
+        public static void TextBox_LostKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (textBox?.DataContext != null)
+            {
+                var bindingExpression = textBox.GetBindingExpression(TextBox.TextProperty);
+                if (bindingExpression != null && bindingExpression.ResolvedSourcePropertyName != null)
+                {
+                    textBox.DataContext.GetType().GetProperty(bindingExpression.ResolvedSourcePropertyName)?.SetValue(textBox.DataContext, textBox.Text);
+                }
+            }
+        }
+    }
+}
